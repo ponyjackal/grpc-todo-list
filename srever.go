611f79d84d28386566bd4fa6d4c905847b3daaad
ponyjackal <pony1125@outlook.com>
2023-10-03 17:59:21 -0700

    feat: add GetTodos

--- srever.go
+++ srever.go
@@ -5,5 +5,17 @@ import todo "github.com/ponyjackal/grpc-todo-list/todo"
 type todoServer struct{}
 
 func (s *todoServer) GetTodos(req *todo.TodoRequest, stream todo.TodoService_GetTodosServer) error {
+	todos := []*todo.Todo{
+		{Id: "1", Title: "Buy groceries", Completed: false},
+		{Id: "2", Title: "Clean the house", Completed: true},
+		{Id: "3", Title: "Walk the dog", Completed: false},
+	}
 
+	for _, todo := range todos {
+		if err := stream.Send(todo); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
